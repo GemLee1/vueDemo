1d171f1788b857bfb6b723cf5c13401e35cf6ece
zhuh-g <zhuh-g@glodon.com>
2019-08-22 18:24:45 +0800

    vue

--- vue-admin-master/src/pages/resourceview/view.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue/types/vue';
-import router from '../../router';
-import store from '../../store';
-import App from './index';
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app');
--- vue-admin-master/src/utils/storage.js
+++
@@ -1,5 +0,0 @@
-import storage from 'good-storage';
-
-export default storage.session;
-
-export { storage };
